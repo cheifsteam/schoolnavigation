32988120b867d667bdf082006ba04b15b9dcd790
cheifsteam <2506111288@qq.com>
2021-12-23 18:13:13 +0800

    初始化项目

---
+++ src/main/java/com/hqd/schoolnavigation/SchoolnavigationApplication.java
@@ -0,0 +1,13 @@
+package com.hqd.schoolnavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolnavigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolnavigationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hqd/schoolnavigation/SchoolnavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqd.schoolnavigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolnavigationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
