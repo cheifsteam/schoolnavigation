5ffc2258ddc0a401edea263c19b680925a0d5efd
cheifsteam <2506111288@qq.com>
2021-12-28 14:39:07 +0800

    初始化混淆盐 生成管理员实体类

---
+++ src/main/java/com/hqd/schoolnavigation/domain/Admin.java
@@ -0,0 +1,53 @@
+package com.hqd.schoolnavigation.domain;
+
+public class Admin {
+    private Integer id;
+
+    private String adminName;
+
+    private String password;
+
+    private String salt;
+
+    private String img;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName == null ? null : adminName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+}
---
+++ src/main/java/com/hqd/schoolnavigation/mapper/AdminMapper.java
@@ -0,0 +1,21 @@
+package com.hqd.schoolnavigation.mapper;
+
+import com.hqd.schoolnavigation.domain.Admin;
+import com.hqd.schoolnavigation.domain.AdminExample;
+import java.util.List;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    List<Admin> selectByExample(AdminExample example);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
