0331f05261dac1c662bba9e603673f4291164704
makiori <1578774771@qq.com>
2021-12-31 16:50:32 +0800

    1.修改前端显示 2.添加前端校验拦截器 3.增加树状插件

--- admin/src/views/admin/category.vue
+++ admin/src/views/admin/category.vue
@@ -1,49 +1,128 @@
 <template>
   <div>
-    <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        新增
-      </button>
-      &nbsp;
-      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-refresh"></i>
-        刷新
-      </button>
-    </p>
 
-    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+    <div class="row">
+      <div class="col-md-4">
+        <p>
+          <button v-on:click="add1()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit"></i>
+            新增一级
+          </button>
+          &nbsp;
+          <button v-on:click="all()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-refresh"></i>
+            刷新
+          </button>
+        </p>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-      <thead>
-      <tr>
-        <th>id</th>
-        <th>父id</th>
-        <th>名称</th>
-        <th>顺序</th>
-        <th>操作</th>
-      </tr>
-      </thead>
+        <table id="level1-table" class="table  table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>名称</th>
+            <th>顺序</th>
+            <th>操作</th>
+          </tr>
+          </thead>
 
-      <tbody>
-      <tr v-for="category in categorys">
-        <td>{{category.id}}</td>
-        <td>{{category.parent}}</td>
-        <td>{{category.name}}</td>
-        <td>{{category.sort}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(category)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+          <tbody>
+          <tr v-for="category in level1" v-on:click="onClickLevel1(category)" v-bind:class="{'active' : category.id === active.id}">
+            <td>{{category.id}}</td>
+            <td>{{category.name}}</td>
+            <td>{{category.sort}}</td>
+            <td>
+              <div class="hidden-sm hidden-xs btn-group">
+                <button v-on:click="edit(category)" class="btn btn-xs btn-info">
+                  <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+                <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
+                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-md-4">
+        <p>
+          <button v-on:click="add2()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit"></i>
+            新增二级
+          </button>
+        </p>
+
+        <table id="level2-table" class="table  table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>名称</th>
+            <th>顺序</th>
+            <th>操作</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <tr v-for="category in level2" v-on:click="onClickLevel2(category)" v-bind:class="{'active' : category.id === active.id}">
+            <td>{{category.id}}</td>
+            <td>{{category.name}}</td>
+            <td>{{category.sort}}</td>
+            <td>
+              <div class="hidden-sm hidden-xs btn-group">
+                <button v-on:click="edit(category)" class="btn btn-xs btn-info">
+                  <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+                <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
+                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-md-4">
+        <p>
+          <button v-on:click="add3()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit"></i>
+            新增三级
+          </button>
+        </p>
+
+        <table id="level-table" class="table  table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>名称</th>
+            <th>顺序</th>
+            <th>操作</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <tr v-for="category in level3">
+            <td>{{category.id}}</td>
+            <td>{{category.name}}</td>
+            <td>{{category.sort}}</td>
+            <td>
+              <div class="hidden-sm hidden-xs btn-group">
+                <button v-on:click="edit(category)" class="btn btn-xs btn-info">
+                  <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+                <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
+                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -55,9 +134,9 @@
           <div class="modal-body">
             <form class="form-horizontal">
               <div class="form-group">
-                <label class="col-sm-2 control-label">父id</label>
+                <label class="col-sm-2 control-label">父分类</label>
                 <div class="col-sm-10">
-                  <input v-model="category.parent" class="form-control">
+                  <p class="form-control-static">{{active.name || "无"}}</p>
                 </div>
               </div>
               <div class="form-group">
@@ -85,32 +164,67 @@
 </template>
 
 <script>
-import Pagination from "../../components/pagination";
 export default {
-  components: {Pagination},
   name: "business-category",
   data: function() {
     return {
       category: {},
       categorys: [],
+      level1: [],
+      level2: [],
+      level3: [],
+      active: {},
     }
   },
   mounted: function() {
     let _this = this;
-    _this.$refs.pagination.size = 5;
-    //_this.list(1);
+    _this.all();
     // sidebar激活样式方法一
     // this.$parent.activeSidebar("business-category-sidebar");
 
   },
   methods: {
     /**
-     * 点击【新增】
+     * 点击【新增一级】
      */
-    add() {
+    add1() {
       let _this = this;
-      _this.category = {};
+      _this.active = {};
+      _this.level2 = [];
+      _this.category = {
+        parent: "00000000"
+      };
       $("#form-modal").modal("show");
+    },
+
+    /**
+     * 点击【新增二级】
+     */
+    add2() {
+      let _this = this;
+      if (Tool.isEmpty(_this.active)) {
+        Toast.warning("请先点击一级分类");
+        return;
+      }
+      _this.category = {
+        parent: _this.active.id
+      };
+      $(".modal").modal("show");
+    },
+
+    /**
+     * 点击【新增二级】
+     */
+    add3() {
+      let _this = this;
+      if (Tool.isEmpty(_this.active)) {
+        Toast.warning("请先点击二级分类");
+        return;
+      }
+      _this.category = {
+        parent: _this.active.id
+      };
+      $(".modal").modal("show");
     },
 
     /**
@@ -125,25 +239,45 @@ export default {
     /**
      * 列表查询
      */
-    list(page) {
+    all() {
       let _this = this;
       Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/category/list', {
-        page: page,
-        size: _this.$refs.pagination.size,
-      }).then((response)=>{
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/admin/admin/category/all').then((response)=>{
         Loading.hide();
         let resp = response.data;
-        _this.categorys = resp.content.list;
-        _this.$refs.pagination.render(page, resp.content.total);
+        _this.categorys = resp.content;
 
+        // 将所有记录格式化成树形结构
+        _this.level1 = [];
+        for (let i = 0; i < _this.categorys.length; i++) {
+          let c = _this.categorys[i];
+          if (c.parent === '00000000') {
+            _this.level1.push(c);
+            for (let j = 0; j < _this.categorys.length; j++) {
+              let child = _this.categorys[j];
+              if (child.parent === c.id) {
+                if (Tool.isEmpty(c.children)) {
+                  c.children = [];
+                }
+                c.children.push(child);
+              }
+            }
+          }
+        }
+
+        _this.level2 = [];
+        // 对当前一级分类中选中的表格触发一次点击事件，以刷新二级菜单列表
+        // 注意：界面的渲染需要等vue绑定好变量后才做，所以加延时100ms
+        setTimeout(function () {
+          $("tr.active").trigger("click");
+        }, 100);
       })
     },
 
     /**
      * 点击【保存】
      */
-    save(page) {
+    save() {
       let _this = this;
 
       // 保存校验
@@ -161,7 +295,7 @@ export default {
         let resp = response.data;
         if (resp.success) {
           $("#form-modal").modal("hide");
-          _this.list(1);
+          _this.all();
           Toast.success("保存成功！");
         } else {
           Toast.warning(resp.message)
@@ -180,12 +314,24 @@ export default {
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
-            _this.list(1);
+            _this.all();
             Toast.success("删除成功！");
           }
         })
       });
+    },
+
+    onClickLevel1(category) {
+      let _this = this;
+      _this.active = category;
+      _this.level2 = category.children;
     }
   }
 }
 </script>
+
+<style scoped>
+.active td {
+  background-color: #d6e9c6 !important;
+}
+</style>
